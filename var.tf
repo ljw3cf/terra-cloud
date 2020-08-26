44029907b605430959be8ccf68e6a93167633e66
ljw3cf <ljw3cf@naver.com>
2020-08-26 10:46:18 +0900

    correct error

--- var.tf
+++ var.tf
@@ -9,6 +9,4 @@ variable "access_key_file" {
 variable "secret_key_file" {
   type = "string"
 }
-#ami_type = "ami-0a9d9cbf7a90d84f6"
-#access_key_file = "/home/student/.ssh/id_rsa.pub"
-#secret_key_file = "/home/student/.ssh/id_rsa"
+
