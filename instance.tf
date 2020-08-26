3ec6cae010ae7701c4fe8fc9478c6aa651773bb0
ljw3cf <ljw3cf@naver.com>
2020-08-26 11:26:03 +0900

    add node2

--- instance.tf
+++ instance.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "node1" {
   ami           = var.ami_type
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "node2" {
+  ami = var.ami_type
+  instance_type = "t2.micro"
+}
